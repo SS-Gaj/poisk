18194ffd19a189a875879dd776811be3b89cb246
Alex <sashagajdarenko@gmail.com>
2018-02-07 19:52:41 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Poisk::Application.config.secret_key_base = '160bb77e92a8af380fcb605baf7af4cf85877c40a6f305e1635f1192463a002fe3b152e14f02625323f18879f77a4af187c84b3dfd5393f9fe9ab8128040ece2'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Poisk::Application.config.session_store :cookie_store, key: '_poisk_session'
